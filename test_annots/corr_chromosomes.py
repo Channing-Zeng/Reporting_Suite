7e793ef4d8471bfb3cd53913fea4d8aaa8fa94de
vladsaveliev <vladsaveliev@me.com>
2014-03-18 19:12:41 +0400

    correct chromoromes

---
+++ test_annots/corr_chromosomes.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+
+def __correct_cromosomes(sample_fpath):
+    result_fpath = sample_fpath + '_tmp'
+
+    with open(sample_fpath) as vcf, open(result_fpath, 'w') as out:
+        for i, line in enumerate(vcf):
+            clean_line = line.strip()
+            if not clean_line or clean_line[0] == '#':
+                out.write(line)
+            else:
+                tokens = line.split()
+                chr_field = tokens[0]
+                line = '\t'.join(['chr' + chr_field] + tokens[1:]) + '\n'
+                out.write(line)
+
+    os.remove(sample_fpath)
+    os.rename(result_fpath, sample_fpath)
+    return sample_fpath
+
+
+if __name__ == '__main__':
+    __correct_cromosomes(sys.argv[0])
