a3df08edd579159cfbb63a7b805b29b1dddfda44
vladsaveliev <vladsaveliev@me.com>
2014-03-31 15:49:18 +0400

    TargetSeq

---
+++ targetseq/html/footer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (C) 2011 Ion Torrent Systems, Inc. All Rights Reserved
+
+print_html_footer()
+{
+echo '
+  <div class="clear"></div>
+  <div class="footer">
+    <a href=http://www.edgebio.com/techinfo/techinfosupport.php>Request Support</a>
+  </div>';
+}
