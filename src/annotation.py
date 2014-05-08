d4e0555bd501b7bf4048960e7724dfffaaaf2136
vladsaveliev <vladsaveliev@me.com>
2014-05-08 19:29:14 +0400

    no dt gatk

--- src/annotation.py
+++ src/annotation.py
@@ -225,7 +225,7 @@ def _gatk(cnf, input_fpath, bam_fpath, work_dir):
 
     ref_fpath = cnf['genome']['seq']
 
-    cmdline = ('{executable} -R {ref_fpath} -T VariantAnnotator'
+    cmdline = ('{executable} -dt NONE -R {ref_fpath} -T VariantAnnotator'
                ' --variant {input_fpath} -o {output_fpath}').format(**locals())
     if bam_fpath:
         cmdline += ' -I ' + bam_fpath
