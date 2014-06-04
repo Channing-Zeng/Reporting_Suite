cb1c9def4847667eba5add7148698e1db2a1b5bd
vladsaveliev <vladsaveliev@me.com>
2014-06-04 21:12:03 +0400

    bcbio test dir; also dbsnpf path for vagrant fix

--- test/bcbio_test_dir/sampl+e1/NGSCat/NGSCat_sampl+e1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /etc/profile.d/modules.sh; module load python/64_2.7.3; module load java; module load bedtools; module load samtools;
-python /group/ngs/src/ngscat/ngscat.py --bams "sampl+e1-ready.bam" --bed "/Users/vlad/vagrant/variantannotation/test/data/sample1.bed" --out NGSCat --reference /ngs/reference_data/genomes/Hsapiens/hg19/seq/hg19.fa --saturation y
-rm -- "/Users/vlad/vagrant/variantannotation/test/bcbio_test_dir/sampl+e1/NGSCat/NGSCat_sampl+e1.sh"
--- test/bcbio_test_dir/sampl+e1/annotation/VarAnn_sampl+e1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /etc/profile.d/modules.sh; module load python/64_2.7.3; module load java; module load bedtools; module load samtools;
-python /group/ngs/src/varannotate.py --var "sampl+e1-mutect.filtered.vcf" --bam "sampl+e1-ready.bam" -o annotation
-rm -- "/Users/vlad/vagrant/variantannotation/test/bcbio_test_dir/sampl+e1/annotation/VarAnn_sampl+e1.sh"
