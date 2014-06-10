6fa4150227fcf051e6713364ed22f911d39b5b4a
vladsaveliev <vladsaveliev@me.com>
2014-06-10 20:02:31 +0400

    refactored summarizers

--- targetcov_summary.py
+++ targetcov_summary.py
@@ -2,10 +2,9 @@
 
 from __future__ import print_function
 import sys
-import os
-from optparse import OptionParser
 from os.path import join
-from genericpath import isfile
+from source.bcbio_utils import file_exists
+from source.logger import info
 from source.utils import critical
 from source.summarize import summarize_cov
 
@@ -15,32 +14,38 @@ if not ((2, 7) <= sys.version_info[:2] < (3, 0)):
              (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
 
 
-def main(argv):  # dir samples.txt report_basedir
-    if len(argv) < 4:
-        critical('Error: provide bcbio dir, list of samples, and report basedir (e.g. VarQC)')
+def main(args):  # dir samples.txt report_basedir
+    if len(args) < 2:
+        critical('Usage: ' + __file__ + ' <bcbio_final_dir> '
+                 '<list_of_sample_names> [report_base_dir (default targetcov)] '
+                 '[vcf_suffic (defauls is -mutect)]')
 
-    out_dirpath = argv[1]
-    samples_fname = argv[2]
-    report_basedir = argv[3]
+    out_dirpath = args[0]
+    samples_fpath = args[1]
+    report_basedir = args[2] if len(args) >= 3 else 'targetcov'
+
     report_suffix = '.targetseq.summary.txt'
-    summary_report_fpath=join(out_dirpath, 'targetcov_summary_report.txt')
+    summary_report_fpath = join(out_dirpath, 'targetcov_summary_report.txt')
     report_fpaths = []
-    with open(samples_fname, 'r') as f:
+
+    with open(samples_fpath, 'r') as f:
         for line in f:
             sample_name = line.strip()
 
             report_fpath = join(out_dirpath, sample_name, report_basedir, sample_name + report_suffix)
-            print(report_fpath)
-            if isfile(report_fpath):
+            info(report_fpath)
+
+            if file_exists(report_fpath):
                 report_fpaths.append(report_fpath)
             else:
-                print(report_fpath + ' does not exist, checking another')
+                info(report_fpath + ' does not exist, checking another')
                 report_fpath = join(out_dirpath, sample_name, report_basedir, sample_name + '-ready' + report_suffix)
-                print(report_fpath)
-                if isfile(report_fpath):
+                info(report_fpath)
+
+                if file_exists(report_fpath):
                     report_fpaths.append(report_fpath)
                 else:
-                    print(report_fpath + ' does not exist! skipping')
+                    info(report_fpath + ' does not exist, skipping')
             print('')
 
     summarize_cov(report_fpaths, summary_report_fpath, report_suffix)
--- varqc_summary.py
+++ varqc_summary.py
@@ -2,10 +2,9 @@
 
 from __future__ import print_function
 import sys
-import os
-from optparse import OptionParser
 from os.path import join
-from genericpath import isfile
+from source.bcbio_utils import file_exists
+from source.logger import info
 from source.utils import critical
 from source.summarize import summarize_qc
 
@@ -15,37 +14,43 @@ if not ((2, 7) <= sys.version_info[:2] < (3, 0)):
              (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
 
 
-def main(argv):  # dir samples.txt report_basedir
-    if len(argv) < 4:
-        critical('Error: provide bcbio dir, list of samples, and report basedir (e.g. VarQC)')
+def main(args):  # dir samples.txt report_basedir
+    if len(args) < 2:
+        critical('Usage: ' + __file__ + ' <bcbio_final_dir> '
+                 '<list_of_sample_names> [report_base_dir (default varqc)] '
+                 '[vcf_suffic (defauls is -mutect)]')
 
-    out_dirpath = argv[1]
-    samples_fname = argv[2]
-    report_basedir = argv[3]
-    vcfsuffix = argv[4]
-    report_suffix='_qc.report'
-    summary_report_fpath=join(out_dirpath, 'varqc_summary_report.txt')
+    out_dirpath = args[0]
+    samples_fpath = args[1]
+    report_basedir = args[2] if len(args) >= 3 else 'varqc'
+    vcfsuffix = args[3] if len(args) >= 4 else '-mutect'
+
+    report_suffix = '_qc.report'
+    summary_report_fpath = join(out_dirpath, 'varqc_summary_report.txt')
     report_fpaths = []
-    with open(samples_fname, 'r') as f:
+
+    with open(samples_fpath, 'r') as f:
         for line in f:
             sample_name = line.strip()
 
             report_fpath = join(out_dirpath, sample_name, report_basedir, sample_name + report_suffix)
-            print(report_fpath)
-            if isfile(report_fpath):
+            info(report_fpath)
+
+            if file_exists(report_fpath):
                 report_fpaths.append(report_fpath)
             else:
-                print(report_fpath + ' does not exist, checking another')
+                info(report_fpath + ' does not exist, checking another')
                 report_fpath = join(out_dirpath, sample_name, report_basedir, sample_name + vcfsuffix + report_suffix)
-                print(report_fpath)
-                if isfile(report_fpath):
+                info(report_fpath)
+
+                if file_exists(report_fpath):
                     report_fpaths.append(report_fpath)
                 else:
-                    print(report_fpath + ' does not exist! skipping')
-            print('')
+                    info(report_fpath + ' does not exist, skipping')
+            info('')
 
     summarize_qc(report_fpaths, summary_report_fpath, report_suffix)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
