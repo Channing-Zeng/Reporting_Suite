e180a61b5dd520f175cc1d210248a3f3c20d3c67
Bushoy, Alla <Alla.Bushoy@astrazeneca.com>
2014-06-18 17:19:47 -0400

    formatted code

--- source/targetcov/copy_number.py
+++ source/targetcov/copy_number.py
@@ -33,7 +33,7 @@ def _get_factors_by_sample(mapped_reads_by_sample):
     return factor_by_sample
 
 
-def _get_factors_by_gene(gene_records,med_depth):
+def _get_factors_by_gene(gene_records, med_depth):
     min_depth_by_genes = defaultdict(list)
     [min_depth_by_genes[gene_record.name].append(gene_record.min_depth) for gene_record in gene_records]
     factors_by_gene = dict()
@@ -41,8 +41,6 @@ def _get_factors_by_gene(gene_records,med_depth):
         med = median(v)
         factors_by_gene[k] = med_depth / med if med else 0
     return factors_by_gene
-
-
 
 
 def get_norm_depths_by_seq_distr(mapped_reads_by_sample, record_by_sample):
@@ -54,11 +52,7 @@ def get_norm_depths_by_seq_distr(mapped_reads_by_sample, record_by_sample):
         for gene_info in [gene_infos for gene_infos in record_by_sample if gene_infos.sample_name == sample]:
             norm_depths[gene_info.name][sample] = gene_info.min_depth * factor
 
-
     return norm_depths
-
-
-
 
 
 def run_copy_number(sample_mapped_reads, gene_depth):
@@ -66,7 +60,7 @@ def run_copy_number(sample_mapped_reads, gene_depth):
 
     med_depth = median([rec.min_depth for rec in list_genes_info])
     norm_depths_by_seq_distr = get_norm_depths_by_seq_distr(sample_mapped_reads, list_genes_info)
-    factors_by_gene = _get_factors_by_gene(list_genes_info,med_depth)
+    factors_by_gene = _get_factors_by_gene(list_genes_info, med_depth)
     norm_depths_by_gene = defaultdict(dict)
     norm2 = defaultdict(dict)
     norm3 = defaultdict(dict)
@@ -74,8 +68,8 @@ def run_copy_number(sample_mapped_reads, gene_depth):
 
     for sample_name in sample_mapped_reads:
         median_depth_by_sample[sample_name] = median([gene_info.min_depth
-                                   for gene_info in list_genes_info
-                                   if gene_info.sample_name == sample_name])
+                                                      for gene_info in list_genes_info
+                                                      if gene_info.sample_name == sample_name])
 
     for gene, norm_depth_by_sample in norm_depths_by_seq_distr.items():
 
@@ -91,17 +85,15 @@ def run_copy_number(sample_mapped_reads, gene_depth):
 
             norm3[gene][sample] = math.log(gene_norm_depth / median_depth_by_sample[sample], 2)
 
-
-    #for gene_info in list_genes_info:
-        #for gene, sample in norm2.items():
-            #for k, v in sample.items():
-                 #print gene + " : " + k + " : " + str(v)
-
-
+    for gene_info in list_genes_info:
+        gene_name = gene_info.name
+        sample = gene_info.sample_name
+        data = [sample, gene_name, gene_info.chrom, gene_info.start_position, gene_info.end_position, gene_info.size,
+                gene_info.min_depth, norm_depths_by_seq_distr[gene_name][sample]]
+        print '\t'.join(map(str, data))
 
 
 def report_row_to_object(gene_depth):
-
     gene_details = []
     for read in gene_depth:
         gene_details.append(GeneDetail(*read))
@@ -121,10 +113,9 @@ class GeneDetail():
         self.min_depth = float(min_depth)
 
     def __str__(self):
-        values = [self.sample_name, self.chrom, self.start_position, self.end_position, self.name, self.type, self.size, self.min_depth]
+        values = [self.sample_name, self.chrom, self.start_position, self.end_position, self.name, self.type, self.size,
+                  self.min_depth]
         return '"' + '\t'.join(map(str, values)) + '"'
-
-
 
 
 if __name__ == '__main__':
