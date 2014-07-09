1bbb22dd1a76b4084ad3e875d59230f0b3183933
vladsaveliev <vladsaveliev@me.com>
2014-07-09 16:17:42 +0400

    checking only existing vcfs

--- source/transaction.py
+++ source/transaction.py
@@ -8,7 +8,7 @@ interruption.
 import os
 import shutil
 import contextlib
-from os.path import exists
+from os.path import exists, join
 from source.utils_from_bcbio import add_suffix
 
 
@@ -29,22 +29,22 @@ def _remove_files(fnames):
 
 
 @contextlib.contextmanager
-def file_transaction(tmp_dir, *rollback_files):
+def file_transaction(cnf, *rollback_files):
     """Wrap file generation in a transaction, moving to output if finishes.
     """
     exts = {".vcf": ".idx", ".bam": ".bai", "vcf.gz": ".tbi"}
-    safe_names, orig_names = _flatten_plus_safe(tmp_dir, rollback_files)
-    _remove_files(safe_names)  # remove any half-finished transactions
+    safe_fpaths, orig_names = _flatten_plus_safe(cnf['work_dir'], rollback_files)
+    _remove_files(safe_fpaths)  # remove any half-finished transactions
     try:
-        if len(safe_names) == 1:
-            yield safe_names[0]
+        if len(safe_fpaths) == 1:
+            yield safe_fpaths[0]
         else:
-            yield tuple(safe_names)
+            yield tuple(safe_fpaths)
     except:  # failure -- delete any temporary files
-        _remove_files(safe_names)
+        _remove_files(safe_fpaths)
         raise
     else:  # worked -- move the temporary files to permanent location
-        for safe, orig in zip(safe_names, orig_names):
+        for safe, orig in zip(safe_fpaths, orig_names):
             if exists(safe):
                 shutil.move(safe, orig)
                 for check_ext, check_idx in exts.iteritems():
@@ -57,12 +57,13 @@ def file_transaction(tmp_dir, *rollback_files):
 def _flatten_plus_safe(tmp_dir, rollback_files):
     """Flatten names of files and create temporary file names.
     """
-    tx_files, orig_files = [], []
+    tx_fpaths, orig_files = [], []
     for fnames in rollback_files:
         if isinstance(fnames, basestring):
             fnames = [fnames]
         for fname in fnames:
             tx_file = add_suffix(fname, 'tmp')
-            tx_files.append(tx_file)
+            tx_fpath = join(tmp_dir, tx_file)
+            tx_fpaths.append(tx_fpath)
             orig_files.append(fname)
-    return tx_files, orig_files
+    return tx_fpaths, orig_files
