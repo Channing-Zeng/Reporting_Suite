87e521fea9e78ffd45f1c57a2f195303a676e2d2
vladsaveliev <vladsaveliev@me.com>
2014-07-11 01:56:20 +0400

    gcc/4.8.3

--- runner_Waltham.sh
+++ runner_Waltham.sh
@@ -3,6 +3,8 @@ date >&2
 source /etc/profile.d/modules.sh >&2
 module unload python
 module load python/64_2.7.3 java perl bedtools samtools >&2
+module unload gcc
+module load gcc/4.8.3
 echo >&2
 echo "$@" >&2
 echo >&2
