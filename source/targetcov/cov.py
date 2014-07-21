73cfbf2cb5ed36642efcb916d8ecabbf292fb9cb
vladsaveliev <vladsaveliev@me.com>
2014-07-21 17:43:06 +0400

    targetcov fix

--- source/targetcov/cov.py
+++ source/targetcov/cov.py
@@ -337,7 +337,7 @@ def _bedcoverage_hist_stats(cnf, bam, bed):
             _total_regions_count += 1
 
             if _total_regions_count > 0 and _total_regions_count % 100000 == 0:
-                info('processed %d00k regions' % _total_regions_count / 100000)
+                info('processed %d00k regions' % (_total_regions_count / 100000))
 
         regions[-1].add_bases_for_depth(depth, bases)
 
