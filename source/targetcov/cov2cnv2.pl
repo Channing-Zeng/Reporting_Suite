123c4c61a51a2a033ea86c3d03a67e9a10dc72f0
vladsaveliev <vladsaveliev@me.com>
2014-08-07 22:30:08 +0400

    cov2cnv2.pl

---
+++ source/targetcov/cov2cnv2.pl
@@ -0,0 +1,106 @@
+#!/usr/bin/perl -w
+
+# Normalize the coverage from targeted sequencing to CNV log2 ratio.  The algorithm assumes the medium
+# is diploid, thus not suitable for homogeneous samples (e.g. parent-child).
+
+use Stat::Basic;
+use Getopt::Std;
+use strict;
+
+our ($opt_c, $opt_a, $opt_i);
+
+getopts( 'aic:' );
+
+my $CNT = shift;
+my %cnt;
+open( CNT, $CNT );
+my @cnt;
+while(<CNT>) {
+    chomp;
+    next if ( /Undetermined/ );
+    next if ( /Sample/ || /Total/ );
+    my @a = split(/\t/);
+    $cnt{$a[0]} = $a[1];
+    push(@cnt, $a[1]);
+}
+close(CNT);
+my $stat = new Stat::Basic;
+my $meanreads = $stat->mean(\@cnt);
+my %factor; # to adjust sequencing coverage
+while(my ($k, $v) = each %cnt) {
+    $factor{ $k } = $meanreads/$v;
+}
+my %raw; # key: gene value: hash of (key: sample; value: raw depth)
+my %norm1; # key: gene value: hash of (key: sample; value: normalized depth by sequencing distribution)
+my %norm1b; # key: gene value: hash of (key: sample; value: normalized depth by gene)
+my %norm2; # key: gene value: hash of (key: sample; value: normalized by gene median scaling log2)
+my %norm3; # key: gene value: hash of (key: sample; value: normalized by sample median scaling log2)
+my %samples;
+my @depth;
+my %data;
+my %loc;
+while( <> ) {
+    next if ( /Whole-/ );
+    next if ( /_CONTROL_/ );
+    next if ( /^Sample/ );
+    my ($sample, $gene, $chr, $s, $e, $t, $len, $depth) = split(/\t/);
+    next if ( $sample eq "Undetermined" );
+    my $k = $opt_a ? join("\t", $gene, $chr, $s, $e, $len) : $gene;
+    $data{ $k }->{ $sample }->{ len } += $len;
+    $loc{ $k }->{ chr } = $chr;
+    $loc{ $k }->{ start } = $s unless( $loc{ $k }->{ start } && $loc{ $k }->{ start } < $s );
+    $loc{ $k }->{ end } = $e unless( $loc{ $k }->{ end } && $loc{ $k }->{ end } > $e );
+    $data{ $k }->{ $sample }->{ cov } += $len*$depth;
+}
+
+while(my($k, $v) = each %data) {
+    while( my($sample, $dv) = each %$v ) {
+        $raw{ $k }->{ $sample } = sprintf("%.2f", $dv->{cov}/$dv->{len});
+        $norm1{ $k }->{ $sample } = sprintf("%.2f", $raw{ $k }->{ $sample }*$factor{ $sample });
+        $samples{ $sample } = 1;
+        push(@depth, $norm1{ $k }->{ $sample } );
+        #print join("\t", $sample, $k, $depth*$factor{ $sample }), "\n";
+    }
+}
+my $meddepth = $stat->median(\@depth);
+my %factor2;  # Gene factor
+while( my ($k, $v) = each %norm1) {
+    my @t = values %$v;
+    $factor2{ $k } = $stat->median(\@t) > 0 ? $meddepth/$stat->median(\@t) : 0;
+}
+my %samplemedian;
+my @samples = keys %samples;
+foreach my $s (@samples) {
+    my @tmp = ();
+    while( my ($k, $v) = each %norm1 ) {
+        push( @tmp, $v->{ $s } );
+    }
+    $samplemedian{ $s } = $stat->median( \@tmp );
+}
+
+while( my ($k, $v) = each %norm1) {
+    foreach my $s (@samples) {
+        $norm1b{ $k }->{ $s } = sprintf("%.2f", $v->{$s} * $factor2{ $k }+0.1);
+        $norm2{ $k }->{ $s } = sprintf("%.2f", log(($v->{$s} * $factor2{ $k }+0.1)/$meddepth)/log(2));
+        $norm3{ $k }->{ $s } = sprintf("%.2f", log(($v->{$s} * $factor2{ $k }+0.1)/$samplemedian{ $s })/log(2));
+    }
+}
+
+print join("\t", qw(Sample Gene Chr Start Stop Length MeanDepth MeanDepth_Norm1 MeanDepth_Norm2 log2Ratio_norm1 log2Ratio_norm2));
+print "\tlog2Ratio_normContr" if ( $opt_c );
+print "\n";
+while( my ($k, $v) = each %norm2) {
+    while( my ($s, $d) = each %$v ) {
+        next if ( $opt_i && $factor2{ $k } == 0);
+        my $t = $opt_a ? $k : "$k\t$loc{$k}->{chr}\t$loc{$k}->{start}\t$loc{$k}->{end}\t$data{$k}->{$s}->{len}";
+        print join("\t", $s, $t, $raw{ $k }->{ $s }, $norm1{ $k }->{ $s }, $norm1b{ $k }->{ $s }, $d, $norm3{ $k }->{ $s });
+        if ( $opt_c ) {
+            my @controls = split(/:/, $opt_c);
+            my @tcntl = map { $norm1b{ $k }->{ $_ } } @controls;
+            my $cntl_ave = $stat->mean( \@tcntl );
+            print "\t", sprintf("%.3f", log($norm1b{ $k }->{ $s }/$cntl_ave)/log(2));
+            #print "\t", sprintf("%.3f", log($norm1b{ $k }->{ $s }/$norm1b{ $k }->{ $opt_c })/log(2));
+        }
+        print "\n";
+    }
+}
