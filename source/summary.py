1c13c78e3890adb683954593b140929edb0d1c77
vladsaveliev <vladsaveliev@me.com>
2014-09-05 16:46:42 +0400

    Defalt sys cnf

--- source/summary.py
+++ source/summary.py
@@ -37,9 +37,9 @@ def process_post_bcbio_args(parser):
     info('BCBio "final" dir: ' + bcbio_final_dir)
 
     config_dirpath = join(bcbio_final_dir, pardir, 'config')
-    for cnf_name in ['run', 'sys']:
+    for file_basename, cnf_name in zip(['run', 'system'], ['run', 'sys']):
         if not opt_dict.get(cnf_name + '_cnf'):
-            cnf_fpath = adjust_path(join(config_dirpath, cnf_name + '_info.yaml'))
+            cnf_fpath = adjust_path(join(config_dirpath, file_basename + '_info.yaml'))
             if not isfile(cnf_fpath) or not verify_file(cnf_fpath):
                 cnf_fpath = Defaults.__dict__[cnf_name + '_cnf']
                 # critical('Usage: ' + __file__ + ' BCBIO_FINAL_DIR [--run-cnf YAML_FILE] [--sys-cnf YAML_FILE]')
