9fbdb47194ec5a6a47166228b6ee99a878f6b472
vladsaveliev <vladsaveliev@me.com>
2014-09-11 22:35:33 +0400

    Indexing only if not exists

--- source/targetcov/bam_file.py
+++ source/targetcov/bam_file.py
@@ -1,8 +1,10 @@
+from os.path import isfile
 from source.calling_process import call
 from source.tools_from_cnf import get_tool_cmdline
 
 
 def index_bam(cnf, bam_fpath):
-    samtools = get_tool_cmdline(cnf, 'samtools')
-    cmdline = '{samtools} index {bam}'.format(**locals())
-    call(cnf, cmdline)
+    if not isfile(bam_fpath + '.bai'):
+        samtools = get_tool_cmdline(cnf, 'samtools')
+        cmdline = '{samtools} index {bam}'.format(**locals())
+        call(cnf, cmdline)
