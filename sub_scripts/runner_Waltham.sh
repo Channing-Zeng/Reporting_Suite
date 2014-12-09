56074c5984b3184a143e6350121f998cc3e9f1dd
vladsaveliev <vladsaveliev@me.com>
2014-12-10 01:01:47 +0300

    source etcprofile.dmodules.sh >&2 in runner_Waltham.sh

--- sub_scripts/runner_Waltham.sh
+++ sub_scripts/runner_Waltham.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 date >&2
+source /etc/profile.d/modules.sh >&2
 module unload python >&2; module load python/64_2.7.3 >&2
 module unload gcc >&2; module load gcc/4.8.3 >&2
 module load java perl bedtools samtools >&2
