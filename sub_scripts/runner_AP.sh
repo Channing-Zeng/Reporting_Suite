87bbcc1cbdb7f58c852051e6e7d9f33626d34ce5
vladsaveliev <vladsaveliev@me.com>
2014-12-10 13:34:08 +0300

    rm snpEff data; AZ_Exome-expanded -> AZ_exome

--- sub_scripts/runner_AP.sh
+++ sub_scripts/runner_AP.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 date >&2
 source /etc/profile.d/modules.sh >&2
-#module unload python >&2; module load python/2.7.3 >&2
 module load java perl bcbio-nextgen >&2
 echo >&2
 echo "$@" >&2
