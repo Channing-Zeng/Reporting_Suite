91b58676db87d6e1cf9ecb15b291ec8476bfcfa6
vladsaveliev <vladsaveliev@me.com>
2015-01-08 22:35:12 +0300

    Support old bedtools fix

--- source/targetcov/cov.py
+++ source/targetcov/cov.py
@@ -745,11 +745,7 @@ def _merge_bed(cnf, bed_fpath, collapse_gene_names=True):
             if len(ts) < 4:
                 return l
             gns = ts[3].split(';')
-            if len(gns) > 1:
-                print l
             l = l.replace(ts[3], ','.join(set(gns)))
-            if len(gns) > 1:
-                print l
             return l
 
         return iterate_file(cnf, output_fpath, fn, 'dstnct')
