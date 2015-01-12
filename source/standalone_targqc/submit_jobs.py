ee96f103bd7e213fc523b755ff1fa5a7f57c6764
vladsaveliev <vladsaveliev@me.com>
2015-01-12 16:30:05 +0300

    reuse for targetcov

--- source/standalone_targqc/submit_jobs.py
+++ source/standalone_targqc/submit_jobs.py
@@ -33,10 +33,9 @@ def run(cnf, bed_fpath, bam_fpaths, main_script_name):
             info('Processing ' + basename(sample.bam))
 
             info('TargetSeq for "' + basename(sample.bam) + '"')
-            reuse = cnf.reuse_intermediate
             # if not sample.targetcov_done():
             #     reuse = False
-            _submit_job(cnf, targetcov_step, sample.name, threads=threads_per_sample, bam=sample.bam, sample=sample.name, reuse=reuse)
+            _submit_job(cnf, targetcov_step, sample.name, threads=threads_per_sample, bam=sample.bam, sample=sample.name)
             summary_wait_for_steps.append(targetcov_step.job_name(sample.name))
 
             if not cnf.reuse_intermediate or not sample.ngscat_done():
@@ -138,7 +137,7 @@ def _prep_steps(cnf, max_threads, threads_per_sample, bed_fpath, main_script_nam
     return targetcov_step, ngscat_step, qualimap_step, targqc_summary_step
 
 
-def _submit_job(cnf, step, sample_name='', wait_for_steps=None, threads=1, reuse=False, **kwargs):
+def _submit_job(cnf, step, sample_name='', wait_for_steps=None, threads=1, **kwargs):
     log_fpath = join(cnf.log_dir, (step.name + ('_' + sample_name if sample_name else '') + '.log'))
 
     if isfile(log_fpath):
@@ -152,8 +151,6 @@ def _submit_job(cnf, step, sample_name='', wait_for_steps=None, threads=1, reuse
     tool_cmdline = get_system_path(cnf, step.interpreter, step.script)
     if not tool_cmdline: sys.exit(1)
     cmdline = tool_cmdline + ' ' + step.param_line.format(**kwargs)
-    if reuse:
-        cmdline += ' --reuse'
 
     hold_jid_line = '-hold_jid ' + ','.join(wait_for_steps or ['_'])
     job_name = step.job_name(sample_name)
