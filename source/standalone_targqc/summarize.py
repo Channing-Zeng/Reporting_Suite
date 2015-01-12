15791d7a54c466831594d34621c91074111846b6
vladsaveliev <vladsaveliev@me.com>
2015-01-12 14:58:40 +0300

    del DISPLAY val for qualimap2

--- source/standalone_targqc/summarize.py
+++ source/standalone_targqc/summarize.py
@@ -90,7 +90,7 @@ def summarize_targqc(cnf, samples, bed_fpath):
                 rows += [[sample.name, sample.qualimap_html_fpath]]
 
             data_file = write_tsv_rows(rows, qualimap_output_dir, 'qualimap_results_by_sample')
-            cmdline = 'DISPLAY= {qualimap} multi-bamqc --data {data_file} -outdir {qualimap_output_dir}'.format(**locals())
+            cmdline = '{qualimap} multi-bamqc --data {data_file} -outdir {qualimap_output_dir}'.format(**locals())
             ret_code = call(cnf, cmdline, exit_on_error=False, return_err_code=True, env_vars=dict(DISPLAY=None))
 
             targqc_full_report.plots = []
