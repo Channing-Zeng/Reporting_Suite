e7b153d392682e898487f7233571bea10428cae8
Alexey Gurevich <alexeigurevich@gmail.com>
2015-02-05 15:33:05 +0300

    Fixed Picard MarkDuplicates "Value was put into PairInfoMap more than once" error

--- source/targetcov/cov.py
+++ source/targetcov/cov.py
@@ -2,13 +2,14 @@
 
 import sys
 import traceback
+import pysam
 from collections import OrderedDict, defaultdict
 from os.path import join, basename, isfile, abspath, realpath
 
 import source
 import source.targetcov
 from source.calling_process import call, call_pipe
-from source.file_utils import intermediate_fname, splitext_plus, verify_file, file_exists, iterate_file
+from source.file_utils import intermediate_fname, splitext_plus, verify_file, file_exists, iterate_file, tmpfile
 from source import logger
 from source.logger import step_greetings, critical, info, err, warn
 from source.reporting import Metric, SampleReport, MetricStorage, ReportSection, write_txt_rows, write_tsv_rows
@@ -449,35 +450,112 @@ def generate_summary_report(
         percent_val = 100.0 * bases / target_info.bases_num if target_info.bases_num else 0
         report.add_record('Part of target covered at least by ' + str(depth) + 'x', percent_val)
 
-    # picard = get_system_path(cnf, 'java', 'picard')
-    # if picard:
-    #     info('Picard duplication metrics for "' + basename(sample.bam) + '"')
-    #     dup_metrics_txt = join(cnf.work_dir, 'picard_dup_metrics.txt')
-    #     cmdline = '{picard} MarkDuplicates' \
-    #               ' I={sample.bam}' \
-    #               ' O=/dev/null' \
-    #               ' METRICS_FILE={dup_metrics_txt}'
-    #     # if not logger.is_local:
-    #     #     cmdline += ' REFERENCE_SEQUENCE={ref_fapth}'
-    #     cmdline = cmdline.format(**locals())
-    #     call(cnf, cmdline, output_fpath=dup_metrics_txt, stdout_to_outputfile=False, exit_on_error=False)
-    #
-    #     if verify_file(dup_metrics_txt, silent=True):
-    #         _parse_picard_dup_report(report, dup_metrics_txt)
-    #
-    #     info('Picard ins size hist for "' + basename(sample.bam) + '"')
-    #     picard_ins_size_hist_pdf = join(cnf.output_dir, 'picard_ins_size_hist.pdf')
-    #     picard_ins_size_hist_txt = join(cnf.output_dir, 'picard_ins_size_hist.txt')
-    #     cmdline = '{picard} CollectInsertSizeMetrics' \
-    #               ' I={sample.bam}' \
-    #               ' O={picard_ins_size_hist_txt}' \
-    #               ' H={picard_ins_size_hist_pdf}'
-    #     # if not logger.is_local:
-    #     #     cmdline += ' REFERENCE_SEQUENCE={ref_fapth}'
-    #     cmdline = cmdline.format(**locals())
-    #     call(cnf, cmdline, output_fpath=picard_ins_size_hist_pdf, stdout_to_outputfile=False, exit_on_error=False)
+    picard = get_system_path(cnf, 'java', 'picard')
+    if picard:
+        info('Picard duplication metrics for "' + basename(sample.bam) + '"')
+        fixed_bam_fpath = _prepare_bam_for_picard(cnf, sample.bam)
+        dup_metrics_txt = join(cnf.work_dir, 'picard_dup_metrics.txt')
+        cmdline = '{picard} MarkDuplicates' \
+                  ' I={fixed_bam_fpath}' \
+                  ' O=/dev/null' \
+                  ' METRICS_FILE={dup_metrics_txt}'
+        # if not logger.is_local:
+        #     cmdline += ' REFERENCE_SEQUENCE={ref_fapth}'
+        cmdline = cmdline.format(**locals())
+        call(cnf, cmdline, output_fpath=dup_metrics_txt, stdout_to_outputfile=False, exit_on_error=False)
+
+        if verify_file(dup_metrics_txt, silent=True):
+            _parse_picard_dup_report(report, dup_metrics_txt)
+
+        info('Picard ins size hist for "' + basename(sample.bam) + '"')
+        picard_ins_size_hist_pdf = join(cnf.output_dir, 'picard_ins_size_hist.pdf')
+        picard_ins_size_hist_txt = join(cnf.output_dir, 'picard_ins_size_hist.txt')
+        cmdline = '{picard} CollectInsertSizeMetrics' \
+                  ' I={fixed_bam_fpath}' \
+                  ' O={picard_ins_size_hist_txt}' \
+                  ' H={picard_ins_size_hist_pdf}'
+        # if not logger.is_local:
+        #     cmdline += ' REFERENCE_SEQUENCE={ref_fapth}'
+        cmdline = cmdline.format(**locals())
+        call(cnf, cmdline, output_fpath=picard_ins_size_hist_pdf, stdout_to_outputfile=False, exit_on_error=False)
 
     return report
+
+
+def _prepare_bam_for_picard(cnf, bam_fpath):
+    def __process_problem_read_aligns(read_aligns):
+        # each alignment: 0:NAME 1:FLAG 2:CHR 3:COORD 4:MAPQUAL 5:CIGAR 6:MATE_CHR 7:MATE_COORD TLEN SEQ ...
+        def __get_key(align):
+            return align.split('\t')[2] + '@' + align.split('\t')[3]
+
+        def __get_mate_key(align):
+            return (align.split('\t')[6] if align.split('\t')[2] != '=' else align.split('\t')[2]) \
+                   + '@' + align.split('\t')[7]
+
+        chr_coord = OrderedDict()
+        for align in read_aligns:
+            key = __get_key(align)
+            if key not in chr_coord:
+                chr_coord[key] = []
+            chr_coord[key].append(align)
+        correct_pairs = []
+        for align in read_aligns:
+            mate_key = __get_mate_key(align)
+            if mate_key in chr_coord:
+                for pair_align in chr_coord[mate_key]:
+                    if read_aligns.index(pair_align) <= read_aligns.index(align):
+                        continue
+                    if __get_mate_key(pair_align) == __get_key(align):
+                        correct_pairs.append((align, pair_align))
+        if not correct_pairs:
+            return []
+        if len(correct_pairs) > 1:
+            # sort by sum of mapping quality of both alignments
+            correct_pairs.sort(key=lambda pair: pair[0].split('\t')[4] + pair[1].split('\t')[4], reverse=True)
+        return [correct_pairs[0][0], correct_pairs[0][1]]
+
+    samtools = get_system_path(cnf, 'samtools')
+    ## TODO if REFERENCE_SEQUENCE is needed for Picard (both in CollectInsertSizeMetrics and MarkDuplicates)
+    ## problem: header should contain exactly the same chromosomes as in REFERENCE_SEQUENCE
+    ## solution:
+    ## header_lines = pysam.view("-H", bam_fpath)
+    ## modify @SQ entries according to cnf.genome.seq + ".fai" or cnf.genome.chr_lengths
+    ## save modified header into COR_HEADER_SAM (plain text file)
+    ## samtools reheader COR_HEADER_SAM INPUT_BAM > CORRECTED_BAM
+
+    #TODO: fix alignments for MarkDuplicates (problem more than 2 alignments per read-pair)
+    # version 1: sort BAM by queryname, process reads which are present more than twice
+    qname_sorted_bam_fpath = intermediate_fname(cnf, bam_fpath, 'qname_sorted')
+    cmdline = '{samtools} sort -n -o {bam_fpath} prefix'.format(**locals())  # queryname sorting, output to stdout, 'prefix' is not used
+    call(cnf, cmdline, qname_sorted_bam_fpath)
+    problem_reads = dict()
+    cur_read_aligns = []
+    for cur_align in pysam.Samfile(qname_sorted_bam_fpath, 'rb'):
+        cur_align = str(cur_align)
+        if cur_read_aligns:
+            if cur_align.split('\t')[0] != cur_read_aligns[0].split('\t')[0]:
+                if len(cur_read_aligns) > 2:
+                    problem_reads[cur_read_aligns[0].split('\t')[0]] = cur_read_aligns
+                cur_read_aligns = []
+        flag = int(cur_align.split('\t')[1])
+        cur_read_aligns.append(cur_align)
+    if len(cur_read_aligns) > 2:
+        problem_reads[cur_read_aligns[0].split('\t')[0]] = cur_read_aligns
+    #TODO if speed up is important and disk space is critical
+    # version 2: read initial BAM and count number of alignments for each read, process the ones with n>2
+
+    for read_id, read_aligns in problem_reads.items():
+        problem_reads[read_id] = __process_problem_read_aligns(read_aligns)
+    # correct input BAM
+    bam_file = pysam.Samfile(bam_fpath, 'rb')
+    fixed_bam_fpath = intermediate_fname(cnf, bam_fpath, 'fixed_for_picard')
+    fixed_bam_file = pysam.Samfile(fixed_bam_fpath, "wb", template=bam_file)
+    for cur_align in bam_file:
+        read_name = str(cur_align).split('\t')[0]
+        if read_name not in problem_reads or str(cur_align) in problem_reads[read_name]:
+            fixed_bam_file.write(cur_align)
+    fixed_bam_file.close()
+    return fixed_bam_fpath
 
 
 def _parse_picard_dup_report(report, dup_report_fpath):
