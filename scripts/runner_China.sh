8c18e52904c0675183c6843955df740a0b00c24c
Alexey Gurevich <alexeigurevich@gmail.com>
2015-11-11 19:35:47 +0300

    Updated config and runner for China

---
+++ scripts/runner_China.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#set -x
+
+DONE_MARKER_FILE=$1
+ERROR_MARKER_FILE=$2
+CMDLINE=$3
+
+date >&2
+hostname >&2
+source /etc/profile.d/modules.sh >&2
+module unload python >&2 2>&2
+module unload gcc >&2 2>&2
+. /home/klrl262/bcbio.sh 0.9.4
+module load gcc/4.7 python/2.7.5 java perl >&2 2>&2
+echo >&2
+echo "${CMDLINE}" >&2
+echo >&2
+echo >&2
+eval "${CMDLINE}"
+status=$?
+if [ "${status}" -ne 0 ]; then
+    echo "ERROR: command exit with code ${status}" >&2
+    echo "${status}">${ERROR_MARKER_FILE}
+else
+    echo "$?">${DONE_MARKER_FILE}
+fi
+echo >&2
+date >&2
+
+#set +x
