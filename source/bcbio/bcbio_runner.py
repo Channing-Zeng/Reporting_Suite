68faea32750d79d8d5498273bb6f48be824b4753
almiheenko <al.miheenko@gmail.com>
2016-02-19 16:54:22 +0300

    Min memory for bam to bigwig

--- source/bcbio/bcbio_runner.py
+++ source/bcbio/bcbio_runner.py
@@ -867,7 +867,8 @@ class BCBioRunner:
                 for sample in self.bcbio_structure.samples:
                     self._submit_job(self.bw_converting,sample.name,
                         sample=sample.name, genome=sample.genome, bam=sample.bam,
-                        wait_for_steps=[self.clin_report.job_name(sample.name) if self.clin_report in self.steps else None])
+                        wait_for_steps=[self.clin_report.job_name(sample.name) if self.clin_report in self.steps else None],
+                        mem_m=getsize(sample.bam) / 1024 / 1024)
             if not self.cnf.verbose:
                 print ''
             if self.cnf.verbose:
