4aa72cf99ec32a9d78defcaf343e5cc59da7c9ca
vladsaveliev <vladsaveliev@me.com>
2016-03-03 01:49:16 +0300

    sort_bed.py: correct usage, check genome resources

--- tools/bed_processing/sort_bed.py
+++ tools/bed_processing/sort_bed.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from os.path import basename
+
 import bcbio_postproc  # do not remove it: checking for python version and adding site dirs inside
 
 import sys
@@ -6,12 +8,12 @@ from optparse import OptionParser
 from source.config import Config
 from source.file_utils import adjust_path
 from source.logger import critical
-from source.prepare_args_and_cnf import determine_sys_cnf
+from source.prepare_args_and_cnf import determine_sys_cnf, check_genome_resources
 from source.targetcov.bam_and_bed_utils import verify_bed, sort_bed
 
 
 def main():
-    parser = OptionParser(usage='Usage: %prog [options] Input_BED_file')
+    parser = OptionParser(usage='Usage: ' + basename(__file__) + ' -o Output_BED_file -g hg19 Input_BED_file')
     parser.add_option('-o', '--output-bed', dest='output_fpath')
     parser.add_option('-g', '--genome', dest='genome')
     (opts, args) = parser.parse_args(sys.argv[1:])
@@ -20,6 +22,8 @@ def main():
         parser.print_help(file=sys.stderr)
         sys.exit(1)
     cnf = Config(opts.__dict__, determine_sys_cnf(opts), {})
+
+    check_genome_resources(cnf)
 
     if not cnf.output_fpath:
         critical(parser.usage)
