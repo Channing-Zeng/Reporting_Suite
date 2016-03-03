8679f289302de8202a85bf1600563a851f09de91
vladsaveliev <vladsaveliev@me.com>
2016-03-03 13:30:29 +0300

    Git dir clean-up: remove ext_tools

--- ext_tools/zhongwu_tools/check_discordant.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-while( <> ) {
-    @a = split(/\t/); 
-    next if ( $a[6] eq "*" ); 
-    print join("\t", @a[2,3,6, 7,8]), "\n" if ( abs($a[8]) > 100000 && $a[6] eq "="); 
-    print join("\t", @a[2,3,6, 7,8]), "\n" if ( $a[6] ne "=" && $a[2] ne $a[6]);
-}
--- ext_tools/zhongwu_tools/runTophat2_mm10.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/users/kdld047/work/NGS/NGS/tophat-2.0.3.Linux_x86_64/tophat -r 20 --mate-std-dev 35 -p 12 -G ~/work/NGS/NGS/genomes/mm10/Annotation/Genes/genes.gtf -o tophat2_out_mouse --rg-id $1 --rg-sample $1 ~/work/NGS/NGS/genomes/mm10/Sequence/BowtieIndex/bowtie2/genome $2 $3 2> mouse_tophat2.err
-
