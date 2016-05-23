a9d07a312af5bbe8a47b703115f526db3b518efd
vladsaveliev <vladsaveliev@me.com>
2016-05-24 01:39:24 +0300

    tools/bed_processing/build_az_exome.sh fix

--- tools/bed_processing/build_az_exome.sh
+++ tools/bed_processing/build_az_exome.sh
@@ -12,7 +12,7 @@
 
 
 
-grep  Ensembl.transcrits.gtf > Ensembl.transcrits.protein_coding.gtf
+grep  Ensembl.transcripts.gtf > Ensembl.transcripts.protein_coding.gtf
 
 grep -wf gene_list.txt /ngs/reference_data/genomes/Hsapiens/hg19/bed/Exons/Ensembl.gtf | grep 'gene' | cut -f1,4,5 | bedtools slop -l 1 -r 0 -i - -g /ngs/reference_data/genomes/Hsapiens/hg19/genome/human.hg19.genome | grch37_to_hg19.py  > genes.bed
 
@@ -25,8 +25,8 @@ grep -wf gene_list.txt /ngs/reference_data/genomes/Hsapiens/hg19/bed/Exons/Ensem
 
 # get the union.bed
 grep -v '^#' /ngs/reference_data/genomes/Hsapiens/hg19/bed/Exons/Ensembl.gtf | grep -w 'transcript' > Ensembl.transcripts.gtf
-grep 'protein_coding\|nonsense_mediated_decay\|miRNA\|IG_C_gene\|IG_D_gene\|IG_J_gene\|IG_V_gene\|TR_C_gene\|TR_D_gene\|TR_J_gene\|TR_V_gene' Ensembl.transcrits.gtf > Ensembl.transcrits.protein_coding.gtf
-cut -f1,4,5 Ensembl.transcrits.protein_coding.gtf > transcripts.txt
+grep 'protein_coding\|nonsense_mediated_decay\|miRNA\|IG_C_gene\|IG_D_gene\|IG_J_gene\|IG_V_gene\|TR_C_gene\|TR_D_gene\|TR_J_gene\|TR_V_gene' Ensembl.transcripts.gtf > Ensembl.transcripts.protein_coding.gtf
+cut -f1,4,5 Ensembl.transcripts.protein_coding.gtf > transcripts.txt
 bedtools slop -l 1 -r 0 -i transcripts.txt -g /ngs/reference_data/genomes/Hsapiens/hg19/genome/human.hg19.genome > transcripts.bed
 grch37_to_hg19.py transcripts.bed | grep -v "^chrH" | grep -v "^chrG" > transcripts.fixedchr.bed
 cd ..
@@ -58,8 +58,8 @@ cut -f7 subtraction.back.annotated.bed | sort -u
 #
 ## get the union.bed
 #grep -v '^#' /ngs/reference_data/genomes/Hsapiens/hg19/bed.20150528/Exons/Ensembl.gtf | grep -w 'transcript' > Ensembl.transcripts.gtf
-#grep '${BIOTYPES}' Ensembl.transcrits.gtf > Ensembl.transcrits.protein_coding.gtf
-#cut -f1,4,5 Ensembl.transcrits.protein_coding.gtf > transcripts.txt
+#grep '${BIOTYPES}' Ensembl.transcripts.gtf > Ensembl.transcripts.protein_coding.gtf
+#cut -f1,4,5 Ensembl.transcripts.protein_coding.gtf > transcripts.txt
 #bedtools slop -l 1 -r 0 -i transcripts.txt -g /ngs/reference_data/genomes/Hsapiens/hg19/genome/human.hg19.genome > transcripts.bed
 #grch37_to_hg19.py transcripts.bed | grep -v "^chrH" | grep -v "^chrG" > transcripts.fixedchr.bed
 #cd ..
