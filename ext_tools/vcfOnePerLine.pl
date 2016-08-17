b9100783e9328da367d9db3d845db39f2e21e807
vladsaveliev <vladsaveliev@me.com>
2016-08-17 16:07:01 +0300

    Update vcfOnePerLine and Remove snpEff examples

--- ext_tools/vcfOnePerLine.pl
+++ ext_tools/vcfOnePerLine.pl
@@ -8,46 +8,53 @@
 # Note: In lines having multiple effects, all other information will be
 #       repeated. Only the 'EFF' field will change.
 #
-#                                                                                                                       Pablo Cingolani 2012
+#															Pablo Cingolani 2012
 #-------------------------------------------------------------------------------
 
 $INFO_FIELD_NUM = 7;
 
 while( $l = <STDIN> ) {
-        # Show header lines
-        if( $l =~ /^#/ ) { print $l; }
-        else {
-                chomp $l;
+	# Show header lines
+	if( $l =~ /^#/ ) { print $l; }
+	else {
+		chomp $l;
 
-                @t = @infos = @effs = (); # Clear arrays
+		@t = @infos = @effs = (); # Clear arrays
 
-                # Non-header lines: Parse fields
-                @t = split /\t/, $l;
+		# Non-header lines: Parse fields
+		@t = split /\t/, $l;
 
-                # Get INFO column
-                $info = $t[ $INFO_FIELD_NUM ];
+		# Get INFO column
+		$info = $t[ $INFO_FIELD_NUM ];
 
-                # Parse INFO column
-                @infos = split /;/, $info;
+		# Parse INFO column
+		@infos = split /;/, $info;
 
-                # Find EFF field
-                $infStr = "";
-                foreach $inf ( @infos ) {
-                        # Is this the EFF field? => Find it and split it
-                        if( $inf =~/^EFF=(.*)/ ) { @effs = split /,/, $1; }
-                        else { $infStr .= ( $infStr eq '' ? '' : ';' ) . $inf; }
-                }
+		# Find EFF field
+		$infStr = "";
+		foreach $inf ( @infos ) {
+			# Is this the EFF field? => Find it and split it
+			if( $inf =~/^EFF=(.*)/ ) {
+				@effs = split /,/, $1;
+				$fieldName = "EFF";
+			} elsif( $inf =~/^ANN=(.*)/ ) {
+				@effs = split /,/, $1;
+				$fieldName = "ANN";
+			} else {
+				$infStr .= ( $infStr eq '' ? '' : ';' ) . $inf;
+			}
+		}
 
-                # Print VCF line
-                if( $#effs <= 0 )       { print "$l\n"; }       # No EFF found, just show line
-                else {
-                        $pre = "";
-                        for( $i=0 ; $i < $INFO_FIELD_NUM ; $i++ ) { $pre .= ( $i > 0 ? "\t" : "" ) . "$t[$i]"; }
+		# Print VCF line
+		if( $#effs <= 0 )	{ print "$l\n"; }	# No EFF found, just show line
+		else {
+			$pre = "";
+			for( $i=0 ; $i < $INFO_FIELD_NUM ; $i++ ) { $pre .= ( $i > 0 ? "\t" : "" ) . "$t[$i]"; }
 
-                        $post = "";
-                        for( $i=$INFO_FIELD_NUM+1 ; $i <= $#t ; $i++ ) { $post .= "\t$t[$i]"; }
+			$post = "";
+			for( $i=$INFO_FIELD_NUM+1 ; $i <= $#t ; $i++ ) { $post .= "\t$t[$i]"; }
 
-                        foreach $eff ( @effs ) { print $pre . "\t" . $infStr . ( $infStr eq '' ? '' : ';' ) . "EFF=$eff" . $post . "\n" ; }
-                }
-        }
+			foreach $eff ( @effs ) { print $pre . "\t" . $infStr . ( $infStr eq '' ? '' : ';' ) . "$fieldName=$eff" . $post . "\n" ; }
+		}
+	}
 }
